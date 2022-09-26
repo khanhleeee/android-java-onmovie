feaeacd3a93cc064265866ac333496b763467f5b
khanhleeee <khanhleee.itvn@gmail.con>
2022-09-26 12:04:45 +0700

    Kas|Build|Conn FireBase

--- app/src/main/java/com/sinhvien/android_java_onmovie/MainActivity.java
+++ app/src/main/java/com/sinhvien/android_java_onmovie/MainActivity.java
@@ -1,14 +1,90 @@
 package com.sinhvien.android_java_onmovie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button loginBtn, logoutBtn, signupBtn;
+    TextView textView;
+
+    private FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+
+        textView = findViewById(R.id.textView2);
+
+        signupBtn = findViewById(R.id.signup_btn);
+        signupBtn.setOnClickListener(v -> {
+            mAuth.createUserWithEmailAndPassword("khanhle@gmail.com", "123456")
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            Toast.makeText(MainActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+        });
+
+        loginBtn = findViewById(R.id.login_btn);
+        loginBtn.setOnClickListener(v -> {
+            mAuth.signInWithEmailAndPassword("khanhle@gmail.com", "123456")
+                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                           FirebaseUser user = mAuth.getCurrentUser();
+                           textView.setText(user.getEmail());
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+
+        logoutBtn = findViewById(R.id.logout_btn);
+        logoutBtn.setOnClickListener(v -> {
+            mAuth.signOut();
+            textView.setText("Log Out");
+        });
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+        if(currentUser != null) {
+            textView.setText(currentUser.getEmail());
+        }
+
     }
 }
