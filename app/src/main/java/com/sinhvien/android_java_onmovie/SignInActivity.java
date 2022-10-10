81b9eaa0857447e20ad416f82e7e53dbd13c6b3f
= <nguyenthituyetngan5142@gmail.com>
2022-10-10 15:04:56 +0700

    chuc nang login

--- app/src/main/java/com/sinhvien/android_java_onmovie/SignInActivity.java
+++ app/src/main/java/com/sinhvien/android_java_onmovie/SignInActivity.java
@@ -1,14 +1,95 @@
 package com.sinhvien.android_java_onmovie;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SignInActivity extends AppCompatActivity {
+
+    EditText etEmail, etPass;
+    Button btnLogin;
+
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        etEmail = findViewById(R.id.etEmailLogin);
+        etPass = findViewById(R.id.etPasswordLogin);
+        btnLogin = findViewById(R.id.btnLogin);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = etEmail.getText().toString().trim();
+                String pass = etPass.getText().toString().trim();
+
+                if(email.isEmpty() || pass.isEmpty()){
+                    Toast.makeText(SignInActivity.this, "Please enter all information !", Toast.LENGTH_SHORT).show();
+                }
+                else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    Toast.makeText(SignInActivity.this, "Please enter the correct email format.!", Toast.LENGTH_SHORT).show();
+                }
+//                else if(!isValidPassword(pass)) {
+//                    Toast.makeText(SignInActivity.this, "Password must be more than 6 characters " +
+//                            "(a-z, A-Z, number, special characters)!", Toast.LENGTH_SHORT).show();
+//                }
+                else {
+                    Toast.makeText(SignInActivity.this, "OK", Toast.LENGTH_SHORT).show();
+                    mAuth = FirebaseAuth.getInstance();
+                    mAuth.signInWithEmailAndPassword("ngannguyen@gmail.com", "123456")
+                            .addOnCompleteListener(SignInActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Intent loginToMain = new Intent(SignInActivity.this, MainActivity.class);
+                                        startActivity(loginToMain);
+                                        Toast.makeText(SignInActivity.this,
+                                                "LOGIN SUCCESSFUL !!!", Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(SignInActivity.this,
+                                                "LOGIN UNSUCCESSFUL!!!", Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            });
+                }
+            }
+        });
+    }
+
+    public static boolean isValidPassword(final String password) {
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{4,}$";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
     }
 }
